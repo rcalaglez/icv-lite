c84374939e8a12270b26a75cbe05b08ab54c3ba8
rcalaglez <rafacala7@gmail.com>
2025-07-14 18:18:50 +0100

    refactor: removing some redundant comments

--- src/hooks/useResumeStore.ts
+++ src/hooks/useResumeStore.ts
@@ -1,15 +1,14 @@
-
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 import { availableTemplates } from "@/templates/templates";
-import type { CVProfile, ResumeData } from "../types/resume";
+import type { CVProfile, ResumeData, Template } from "../types/resume";
 import { sampleResumes } from "@/data/resumeExample";
 
 interface ResumeStore {
   profiles: CVProfile[];
   getProfileById: (id: string) => CVProfile | undefined;
   updateProfileData: (id: string, data: ResumeData) => void;
-  createProfile: () => string; // Devuelve el ID del nuevo perfil
+  createProfile: () => string;
   deleteProfile: (id: string) => void;
   updateProfileName: (id: string, name: string) => void;
   updateProfileTemplate: (id: string, template: Template) => void;
@@ -20,7 +19,7 @@ interface ResumeStore {
 const useResumeStore = create<ResumeStore>()(
   persist(
     (set, get) => ({
-      profiles: sampleResumes, // Inicializamos directamente con los datos de ejemplo
+      profiles: sampleResumes,
 
       getProfileById: (id: string) => {
         return get().profiles.find((p) => p.id === id);
@@ -77,7 +76,9 @@ const useResumeStore = create<ResumeStore>()(
       updateProfileName: (id: string, name: string) => {
         set((state) => ({
           profiles: state.profiles.map((profile) =>
-            profile.id === id ? { ...profile, name, updatedAt: new Date().toISOString() } : profile
+            profile.id === id
+              ? { ...profile, name, updatedAt: new Date().toISOString() }
+              : profile
           ),
         }));
       },
@@ -85,7 +86,9 @@ const useResumeStore = create<ResumeStore>()(
       updateProfileTemplate: (id: string, template: Template) => {
         set((state) => ({
           profiles: state.profiles.map((profile) =>
-            profile.id === id ? { ...profile, template, updatedAt: new Date().toISOString() } : profile
+            profile.id === id
+              ? { ...profile, template, updatedAt: new Date().toISOString() }
+              : profile
           ),
         }));
       },
@@ -135,7 +138,6 @@ const useResumeStore = create<ResumeStore>()(
     }),
     {
       name: "icv-resume-storage",
-      
     }
   )
 );
